3a351aa3454cf4ca4ce853a6ef1818c37b824ff8
Sayan Roy <sayan98.roy@gmail.com>
2023-04-17 01:17:56 +0530

    adding the assembly

---
+++ Sayan.SentimentAnalysis/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Sayan_SentimentAnalysis;
+
+//Console.WriteLine("Hello, World!");
+
+//Load sample data
+
+
+//Load model and predict output
+Console.Write("Enter Review: ");
+var review = Console.ReadLine();
+var sampleData = new SentimentModel.ModelInput()
+{
+    Col0 = $"{review}",
+};
+var result = SentimentModel.Predict(sampleData);
+string finalResult = result.PredictedLabel == 0 ? "Negative" : "Positive";
+Console.WriteLine($"Sentiment: {finalResult.ToString()}");
